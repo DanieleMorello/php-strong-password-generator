7b9928663f9e3c4167a6f63bee9aa86eab5e7f2d
DanieleMorello <danielemorello@live.it>
2023-05-06 18:39:20 +0200

    Move the logic into a functions.php file

--- index.php
+++ index.php
@@ -1,4 +1,7 @@
 <?php
+
+require __DIR__ . '/./functions.php';
+
 $carachter_psw = [
   [
     'a',
@@ -93,33 +96,6 @@ $carachter_psw = [
     '=',
   ],
 ];
-
-/**
- * Genera una password casuale utilizzando un insieme di caratteri possibili.
- *
- * @param array $character_psw Un array di array, in cui ogni sottovettore contiene un insieme di caratteri possibili da utilizzare nella password.
- * @param int $n_characters_psw La lunghezza desiderata della password generata.
- *
- * @return string La password generata.
- */
-function generate_psw($character_psw, $n_characters_psw)
-{
-  // Inizializza una stringa vuota che conterrà la password generata
-  $psw_generated = '';
-
-  // Ripeti il numero di volte specificato dalla lunghezza desiderata della password
-  foreach (range(1, $n_characters_psw) as $i) {
-    // Seleziona un sottovettore casuale dell'array $character_psw
-    $sub_arr = $character_psw[rand(0, count($character_psw) - 1)];
-    // Seleziona un carattere casuale dal sottovettore selezionato
-    $character = $sub_arr[rand(0, count($sub_arr) - 1)];
-    // Concatena il carattere alla stringa $psw_generated
-    $psw_generated .= $character;
-  }
-
-  // Restituisci la password generata come stringa
-  return $psw_generated;
-}
 
 ?>
 
