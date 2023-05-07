0f420c0a3f30b17a9b01f7bc05f5dd80b83da78a
DanieleMorello <danielemorello@live.it>
2023-05-07 16:29:55 +0200

    Add comment at function resetForm()

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,12 @@
+/**
+ * Questa funzione resetta un form impostando il valore di un campo di input con ID "n_characters_psw" a una stringa vuota
+ * e nascondendo un elemento con ID "password-container"
+ */
 function resetForm() {
+  // Trova l'elemento di input con ID "n_characters_psw" e imposta il suo valore a una stringa vuota
   document.getElementById("n_characters_psw").value = "";
+
+  // Trova l'elemento HTML con ID "password-container" e imposta la sua proprietà "display" a "none"
+  // Ciò nasconde l'elemento dalla vista sulla pagina web
   document.getElementById("password-container").style.display = "none";
 }
